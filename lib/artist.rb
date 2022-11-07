e4a2218a7f7b3ddfe9b9ef460aed559da4916caf
Yana <yana.videoeditor.gmail.com>
2022-11-06 21:23:17 -0700

    commit tree eca666c389018ccf4b902283ae39d98d8cca181d

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,32 +4,27 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
 
+  @@artists = []
+  
   def initialize
     @@artists << self
     @songs = []
   end
 
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    @@artists.detect { |a| a.name == name }
   end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
